3efa955d8c343ecfd79d1a555c1a646d957cc6cf
kasakigaming <minefc1111@gmail.com>
2023-10-15 19:42:53 +0700

    hehe

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+function showSubMenu() {
+    var subMenu = event.currentTarget.querySelector('.sub-menu');
+    subMenu.style.display = "block";
+    
+  }
+
+  function hideSubMenu() {
+    var subMenu = event.currentTarget.querySelector('.sub-menu');
+    subMenu.style.display = "none";
+  }
+
+  
